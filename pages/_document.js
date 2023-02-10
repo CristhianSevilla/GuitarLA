d620ad17ab5d3e12077b3d1f398f78bbd69ab77e
Cristhian Sevilla <crissev17@gmail.com>
2023-02-10 14:56:08 -0600

    Agregar hojas de estilos externas fuente y normalize

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,31 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Link from 'next/link'
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+
+    return (
+
+        <Html>
+
+            <Head>
+
+                <meta name="description" content='Guitar Los Angeles, Venta de guitarras y blog de música' />
+                
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" cross0rigin = {'true'} />
+                <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;700;900&display=swap" rel="stylesheet" />
+
+                <link rel='stylesheet' href='https://necolas.github.io/normalize.css/8.0.1/normalize.css' />
+            </Head>
+
+            <body>
+                <Main/>
+                <NextScript/>
+
+            </body>
+
+        </Html>
+
+
+    )
 }
