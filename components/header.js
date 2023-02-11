880e3b8398d74aa6bb57787045577a5ba016fdbf
Cristhian Sevilla <crissev17@gmail.com>
2023-02-11 14:58:54 -0600

    Imagenes en Next

--- components/header.js
+++ components/header.js
@@ -1,7 +1,14 @@
+import Image from 'next/image'
+import logo from '../public/img/logo.svg'
 
 function Header() {
   return (
-    <div>Header</div>
+    
+    <header>
+      <div className="contenedor">
+        <Image src={logo.src} width={300} height={40} alt='logotipo GuitarLa' />
+      </div>
+    </header>
   )
 }
 
