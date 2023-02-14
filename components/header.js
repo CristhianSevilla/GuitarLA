ee0362ab6a7701b9966dd5f4b81d3a59a0fcf0d8
Cristhian Sevilla <crissev17@gmail.com>
2023-02-14 14:07:17 -0600

    CSS al Header

--- components/header.js
+++ components/header.js
@@ -7,7 +7,7 @@ function Header() {
   return (
 
     <header className= {styles.header}>
-      <div className="contenedor">
+      <div className={`contenedor ${styles.barra}`}>
 
         <Image src='/img/logo.svg' width={300} height={40} alt='logotipo GuitarLa' />
 
