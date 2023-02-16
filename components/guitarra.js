d71ed7170c5b8011549b5b69a5f0f2b063af9d37
Cristhian Sevilla <crissev17@gmail.com>
2023-02-15 18:43:17 -0600

    Cargar imagenes externas (claudinary) y crear el link para ver información de guitarra

--- components/guitarra.js
+++ components/guitarra.js
@@ -1,12 +1,28 @@
+import Image from "next/image"
+import Link from "next/link"
 
-function Guitarra ({guitarra}) {
+function Guitarra({ guitarra }) {
 
-    const {nombre, descripcion, precio, imagen, url} = guitarra
+    const { nombre, descripcion, precio, imagen, url } = guitarra
 
     return (
-       <>
-            <div>{nombre}</div>
-       </>
+        <>
+            <div>
+                <Image src={imagen.data.attributes.formats.medium.url} width={400} height={400} alt={`Imagen de la guitarra ${nombre}`} />
+
+                <div>
+                    <div>{nombre}</div>
+                    <div>{descripcion}</div>
+                    <div>${precio}</div>
+                    <Link href={`guitarras/${url}`}>
+                        Ver producto
+                    </Link>
+
+
+                </div>
+            </div>
+
+        </>
     )
 }
 
