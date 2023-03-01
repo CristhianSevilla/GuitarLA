9147f71c13f5c3a97923e19cec847e17f22794ed
Cristhian Sevilla <crissev17@gmail.com>
2023-03-01 16:40:44 -0600

    Mostrando productos del carrito

--- pages/carrito.js
+++ pages/carrito.js
@@ -1,30 +1,52 @@
 import Layout from "@/components/layout"
 import styles from "../styles/carrito.module.css"
+import Producto from "./guitarras/[url]"
+import Image from "next/image"
 
 
-function Carrito() {
-  return (
-    <Layout
-        title="Carrito de compras"
-    >
+function Carrito({ carrito }) {
+    return (
+        <Layout
+            title="Carrito de compras"
+        >
 
-        <main className="contenedor">
-            <h1 className="heading">Carrito</h1>
+            <main className="contenedor">
+                <h1 className="heading">Carrito</h1>
 
-            <div className={styles.contenido}>
-                <div className={styles.carrito}>
-                    <h2>Artículos</h2>
+                <div className={styles.contenido}>
+                    <div className={styles.carrito}>
+                        <h2>Artículos</h2>
+
+                        {carrito.length === 0 ? "Carrito Vacio" : (
+
+                            carrito.map(producto => (
+                                <div key={producto.id} className={styles.producto} >
+                                    <div>
+                                        <Image width={250} height={400} src={producto.imagen} alt={`Guitarra ${producto.nombre}`} />
+                                    </div>
+                                    <div>
+                                        <p className={styles.nombre} >{`Guitarra: ${producto.nombre}`}</p>
+                                        <p className={styles.precio} >Precio: $<span>{producto.precio}</span></p>
+                                        <p className={styles.cantidad} >Cantidad: {producto.cantidad}</p>
+                                        <p className={styles.subtotal}>Subtotal: $<span>{producto.cantidad * producto.precio}</span></p>
+
+                                    </div>
+                                </div>
+                            ))
+
+                        )}
+
+                    </div>
+
+                    <aside className={styles.resumen}>
+                        <h3>Resumen del pedido</h3>
+                        <p>Total a pagar:</p>
+                    </aside>
                 </div>
+            </main>
 
-                <aside className={styles.resumen}>
-                    <h3>Resumen del pedido</h3>
-                    <p>Total a pagar:</p>
-                </aside>
-            </div>
-        </main>
-
-    </Layout>
-  )
+        </Layout>
+    )
 }
 
 export default Carrito
